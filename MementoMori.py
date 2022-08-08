ce2e7fa1359d7914bd0f90f9c425f080899b54fe
thatscringebro <thatscringebro@tutanota.com>
2022-08-08 16:44:02 -0400

    Leger avancement

--- MementoMori.py
+++ MementoMori.py
@@ -3,12 +3,17 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 
-class PongGame(Widget):
-    pass
+quote = "It is not that we have a short time to live, but that we waste a lot of it. Life is long enough, and a sufficiently generous amount has been given to us for the highest achievements if it were all well invested"
 
-class TestApp(App):
+class MementoMori(Widget):
+    my_label = Label()
+    my_label.text = quote
+    my_label.color = '#FFFFFF'
+    
+class MementoApp(App):
     def build(self):
-        return PongGame()
+        return MementoMori()
 
-TestApp().run()
+MementoApp().run()
