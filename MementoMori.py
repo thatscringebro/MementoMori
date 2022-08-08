db362ccdca3519a9de9e1567c807189f3fc2e65a
thatscringebro <thatscringebro@tutanota.com>
2022-08-08 16:06:35 -0400

    Let there be code

---
+++ MementoMori.py
@@ -0,0 +1,2 @@
+# It is not that we have a short time to live, but that we waste a lot of it. Life is long enough, and a sufficiently generous amount has been given to us for the highest achievements if it were all well invested
+# SENECA
